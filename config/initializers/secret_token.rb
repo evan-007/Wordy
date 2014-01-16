15e817f6044a002bd280f32ac3d906b1fa7834ee
Evan Lloyd <eulloyd@gmail.com>
2014-01-17 08:13:05 +0900

    Generates empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wordy::Application.config.secret_key_base = 'a70d025f1d1100f28bd4d5128060914d85088513e3617d7b4dc20c34ce1da74ff0763214ab282c62563cbda06dc37d8867b59a792052df792ee1e128a2055b4f'
