c272e0a66a81f8606d0945c4e17e7dd4acf3079e
Evan Lloyd <eulloyd@gmail.com>
2014-01-19 20:06:05 +0900

    fixes test

--- spec/features/scanning_text_spec.rb
+++ spec/features/scanning_text_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature "Using the text tool" do
 	scenario "returns a list of NGSL words" do
 		visit 'text_tool_path'
-		fill_in(:input), with: 'here is some text alphabet'
+		fill_in 'input', with: 'here is some text alphabet'
 		click_button "submit"
 		expect(page).to have_content("These words are not on the NGSL:")
 	end
