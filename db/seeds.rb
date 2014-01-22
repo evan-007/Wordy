38153635010728ec124d4f22de7d11da681124f2
Evan Lloyd <eulloyd@gmail.com>
2014-01-22 10:13:05 +0900

    changes production config for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+categories = Category.create([ { name: 'Beginner'}, {name: 'Intermediate'}, {name: 'Advanced'}
+	])
+
+
+require 'csv'
+words = 0 #value is the column in the .csv
+definitions = 1
+@counter = 0
+CSV.foreach("#{Rails.root}"+"/lib/ngsl-utf8.csv") do |row|
+	Word.create(name: row[words], definition: row[definitions])
+	@counter += 1
+	if @counter > 60
+		exit
+	end
+end
+
