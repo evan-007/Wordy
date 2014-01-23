40a54ee5daeb563229325666183cf9a7f390e126
Evan Lloyd <eulloyd@gmail.com>
2014-01-23 10:23:30 +0900

    Builds rake task for list creation

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 categories = Category.create([ { name: 'Beginner'}, {name: 'Intermediate'}, {name: 'Advanced'}
 	])
 
+lists = List.create([ {name: 'Beginner', id: 1}, { name: 'Intermediate', id: 2}, {name: 'Advanced', id: 3}  ])
+
 
 require 'csv'
 words = 0 #value is the column in the .csv
@@ -22,4 +24,12 @@ CSV.foreach("#{Rails.root}"+"/lib/ngsl-utf8.csv") do |row|
 	end
 end
 
-
+Word.all.each do |word|  #why doesn't this work as a seed task, but works in rails console?
+	if word.categories.first.id == 1
+		Wordlist.create(list_id: 1, word_id: word.id)
+	elsif word.categories.first.id == 2
+		Wordlist.create(list_id: 2, word_id: word.id)
+	elsif word.categories.first.id == 3
+		Wordlist.create(list_id: 3, word_id: word_id)
+	end
+end
