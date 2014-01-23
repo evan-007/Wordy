6f029974d96a924b5660449bdaf0988fcd155252
Evan Lloyd <eulloyd@gmail.com>
2014-01-23 10:41:55 +0900

    adds test for viewing system lists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 	before_action :get_user, only: [:show, :edit, :update]
+  before_action :get_sys_lists, only: [:show]
   def show
   end
 
@@ -16,5 +17,9 @@ class UsersController < ApplicationController
 
     def get_user
     	@user = User.find(params[:id])
+    end
+
+    def get_sys_lists
+      @lists = List.all.where(params[:id] < '4')
     end
 end
