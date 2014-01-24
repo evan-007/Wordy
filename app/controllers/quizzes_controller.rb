6481a0a2b2a56dc9a20707983071eb1885843069
Evan Lloyd <eulloyd@gmail.com>
2014-01-24 15:29:56 +0900

    Updates controller and show

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -23,9 +23,8 @@ class QuizzesController < ApplicationController
 		if @quiz.save
 			@quiz.category.words.each do |w| #refactor this mess out of the controller!!!
 				page = Nokogiri::HTML(open("http://bnc.bl.uk/saraWeb.php?qy=#{w.name}&mysubmit=Go"))
-				example = page.css('html body div#solutions p')[1].text
-				q = Question.create(word: w.name, quiz_id: @quiz.id, text: example, answer: [w.name])
-				q.answer << @quiz.category.words.sample(3) #this doesn't work
+				example = page.css('html body div#solutions p')[1].text.html_safe #this needs some way to not return strange characters instead of '' and ""
+				q = Question.create(word: w.name, quiz_id: @quiz.id, text: example, answer: [w.name].push(@quiz.category.words.sample(3)))
 				end
 			flash[:notice] = "Quiz created"
 			redirect_to [@user, @quiz]
