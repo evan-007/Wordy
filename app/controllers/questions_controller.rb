8686391c5160d59d6961444a40a864cc443eeb04
Evan Lloyd <eulloyd@gmail.com>
2014-01-27 15:30:26 +0900

    completes quiz implementation through edit action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,19 @@
 class QuestionsController < ApplicationController
 	before_action :get_user!
 	before_action :get_quiz!
-	
+	before_action :get_question!
+
 	def edit
-		@question = Question.find(params[:id])
+	end
+
+	def update
+		if @question.update(question_params) && @question.guess == @question.word
+				flash[:notice] = "Good job, here's the next one"
+				render :edit
+		else 
+			flash[:notice] = "Read the next one a bit closer"
+			render :edit
+		end
 	end
 
 
@@ -14,5 +24,13 @@ class QuestionsController < ApplicationController
 
 	  def get_quiz!
 	  	@quiz = current_user.quizzes.find(params[:id])
+	  end
+
+	  def get_question!
+	  	@question = Question.find(params[:id])
+	  end
+
+	  def question_params
+	  	params.require(:question).permit(:guess)
 	  end
 end
