dc11913094ed8cb779eba7ffab764ec03bea0267
Evan Lloyd <eulloyd@gmail.com>
2014-01-27 10:41:55 +0900

    fixes answer array

--- lib/question_builder.rb
+++ lib/question_builder.rb
@@ -4,6 +4,10 @@ require 'open-uri'
 def build_questions(w)
 	page = Nokogiri::HTML(open("http://bnc.bl.uk/saraWeb.php?qy=#{w.name}&mysubmit=Go"))
 	example = page.css('html body div#solutions p')[1].text.html_safe #this needs some way to not return strange characters instead of '' and ""
-	q = Question.create(word: w.name, quiz_id: @quiz.id, text: example, answer: [w.name].push(@quiz.category.words.sample(3)))
+	answer_array = [w.name, ]
+	@quiz.category.words.sample(3).each do |word|
+		answer_array << word.name
+	end
+	answer_array
+	q = Question.create(word: w.name, quiz_id: @quiz.id, text: example, answer: answer_array)
 end
-
