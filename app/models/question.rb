f8320f8d86840ce59718e69fe58a7fac51c8f181
Evan Lloyd <eulloyd@gmail.com>
2014-01-28 13:41:41 +0900

    Fixes repeat words on answer samples

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
 	belongs_to :quiz
 	serialize :answer,Array
-	before_update :grade
+	after_update :grade
 	acts_as_list scope: :quiz
 
 
@@ -10,8 +10,6 @@ class Question < ActiveRecord::Base
 		def grade
 			if self.guess == self.word
 				self.correct = true
-			elsif self.guess != self.word
-			    self.correct = false
 			end
 		end
 end
--- lib/question_builder.rb
+++ lib/question_builder.rb
@@ -5,7 +5,7 @@ def build_questions(w)
 	page = Nokogiri::HTML(open("http://bnc.bl.uk/saraWeb.php?qy=#{w.name}&mysubmit=Go"))
 	example = page.css('html body div#solutions p')[1].text.html_safe #this needs some way to not return strange characters instead of '' and ""
 	answer_array = [w.name]
-	@quiz.category.words.sample(3).each do |word|
+	@quiz.category.words.where.not(name: w.name).sample(3).each do |word|
 		answer_array << word.name
 	end
 	answer_array
