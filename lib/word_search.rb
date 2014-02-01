4cfe5049a01d83c5ee656bcaaa6c6fdbeb2f71d4
Evan Lloyd <eulloyd@gmail.com>
2014-02-01 21:01:10 +0900

    changes wordtool results layout

--- lib/word_search.rb
+++ lib/word_search.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'open-uri'
+
 class WordSearch
 
   def search(text)
@@ -18,4 +21,17 @@ class WordSearch
   	end
   	answer
   end
+
+  def get_def(word)
+    @ref = "/xml/"
+    page = Nokogiri::HTML(open("http://www.dictionaryapi.com/api/v1/references/learners/xml/#{word}?key=3b84ccd5-22a9-44ce-b22e-e5ffbef8e3b1"))
+    definition = page.css('entry_list entry dt').first.text
+      return definition
+  end
+
+  private
+
+    def key
+      @key = '3b84ccd5-22a9-44ce-b22e-e5ffbef8e3b1'
+    end
 end
