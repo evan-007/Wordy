e8359dd744cceded6352fc5682bc3acadf1990dc
Evan Lloyd <eulloyd@gmail.com>
2014-02-05 20:17:27 +0900

    Add test for 10 quiz mailings

--- spec/features/sending_quiz_email_spec.rb
+++ spec/features/sending_quiz_email_spec.rb
@@ -1,14 +1,27 @@
 require 'spec_helper'
 
 feature "Sending email" do
-
-	scenario "sends email after 5 quiz completions" do
+	before do
 		@user = create(:user)
 		@category = create(:category)
 		@word = create(:word)
 		@word_category = create(:word_category, word: @word, category: @category)
 		@example = create(:example, word: @word)
+	end
+
+	scenario "sends email after 5 quiz completions" do
 		4.times do
+			create(:quiz, user: @user, category: @category, finished: true)
+		end
+		@quiz = create(:quiz, user: @user, category: @category, finished: false)
+		@quiz.update(finished: true)
+
+		expect(open_last_email).to be_delivered_from "admin@somesite.com"
+		expect(open_last_email).to be_delivered_to @user.email
+	end
+
+	scenario "sends email after 10 quiz completions" do
+		9.times do
 			create(:quiz, user: @user, category: @category, finished: true)
 		end
 		@quiz = create(:quiz, user: @user, category: @category, finished: false)
