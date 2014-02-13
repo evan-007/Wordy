385aa5f778eeed6073777e49c5c852b8e1b8cc2c
Evan <eulloyd@gmail.com>
2014-02-13 06:12:39 +0000

    Add active_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Wordy::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :wordlists, only: [:create, :destroy]
   resources :words 
