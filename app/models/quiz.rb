26ddc7fa790499c7b5ea21623ba89c073be0b8c8
Evan <eulloyd@gmail.com>
2014-02-18 02:23:28 +0000

    Clean up quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,20 +1,15 @@
 class Quiz < ActiveRecord::Base
 	belongs_to :user
-	belongs_to :category #delete
 	belongs_to :list
 	validates :name, presence: true
 	validates :kind, presence: true
 	has_many :questions
   after_create :get_examples 
- #   if: Proc.new { |quiz| quiz.kind == 1 } #this works, but read about procs
-#  after_create :kind_2,
-#   if: Proc.new { |quiz| quiz.kind == 2 }
-#  after_create :kind_3,
-#    if: Proc.new { |quiz| quiz.kind == 3 }
-  self.per_page = 10
 	after_update :count, if: :state_changed?
-	scope :finished, -> { where(state: 'finished') }
 	after_update :state_check
+  self.per_page = 10
+  scope :finished, -> { where(state: 'finished') }
+
   
   
 	state_machine initial: :ready do
@@ -69,35 +64,7 @@ class Quiz < ActiveRecord::Base
 		    end
 	    end
 	end
-  
-  def kind_2
-    @id = self.id
-		self.list.words.each do |w|
-			answer_array = [w.name]
-			self.list.words.where.not(name: w.name).sample(3).each do |word|
-        answer_array << word.name
-				end
-			answer_array
-      q = Question.create(word: w.name, quiz_id: @id, text: w.definition, answer: answer_array)
-    end
-  end
-  
-  def kind_3
-    @id = self.id
-		self.list.words.each do |w|
-			answer_array = [w.name]
-			self.list.words.where.not(name: w.name).sample(3).each do |word|
-        answer_array << word.name
-				end
-			answer_array
-      rand_example = w.examples.shuffle[0].text.sub(/([^\s]+\s+[^\s]+[\s])/, '')
-      example = rand_example
-      q = Question.create(word: example, quiz_id: @id, text: example.split(' ').shuffle.join(' '), answer: answer_array)
-    end
-  end
-  
-  
-
+ 
     private
 		def count
 			if self.user.quizzes.finished.size % 5 == 0
